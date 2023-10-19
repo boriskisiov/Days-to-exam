24a5d6862b29d61f4694106e74d472f4be9861ca
boriskisiov <boriskisiov@gmail.com>
2023-10-19 10:18:02 +0300

    Add project files.

---
+++ Days to exam/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Days_to_exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            for(int i = days; i >= 1; i -= 1)
+            {
+                Console.WriteLine($"{i} days before exam", i);
+            }
+            Console.WriteLine("The exam has come");
+        } 
+    }
+}
